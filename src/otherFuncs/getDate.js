dabd141fdfdbf0071d5b1359d7a3053c74f8db00
Vladislav Teleshov <Necro01@mail.ru>
2022-08-10 13:32:53 +0300

    fix bug date

--- src/otherFuncs/getDate.js
+++ src/otherFuncs/getDate.js
@@ -1,22 +1,34 @@
 const getDate = () => {
-    const dateCheckIn = new Date()
-    dateCheckIn.setDate(dateCheckIn.getDate());
+  const dateCheckIn = new Date();
+  dateCheckIn.setDate(dateCheckIn.getDate());
 
-    const year = dateCheckIn.getFullYear();
-    const month = (dateCheckIn.getMonth() + 1) < 10 ? 0 + (dateCheckIn.getMonth() + 1).toString() : dateCheckIn.getMonth() + 1;
-    const day = dateCheckIn.getDate()
+  const year = dateCheckIn.getFullYear();
+  const month =
+    dateCheckIn.getMonth() + 1 < 10
+      ? 0 + (dateCheckIn.getMonth() + 1).toString()
+      : dateCheckIn.getMonth() + 1;
+  const day =
+    dateCheckIn.getDate() < 10
+      ? 0 + dateCheckIn.getDate().toString()
+      : dateCheckIn.getDate();
 
-    return `${year}-${month}-${day}`
-}
-export default getDate
+  return `${year}-${month}-${day}`;
+};
+export default getDate;
 
 export const getDateOut = () => {
-    const dateCheckIn = new Date()
-    dateCheckIn.setDate(dateCheckIn.getDate() + 1);
+  const dateCheckIn = new Date();
+  dateCheckIn.setDate(dateCheckIn.getDate() + 1);
 
-    const year = dateCheckIn.getFullYear();
-    const month = (dateCheckIn.getMonth() + 1) < 10 ? 0 + (dateCheckIn.getMonth() + 1).toString() : dateCheckIn.getMonth() + 1;
-    const day = dateCheckIn.getDate()
+  const year = dateCheckIn.getFullYear();
+  const month =
+    dateCheckIn.getMonth() + 1 < 10
+      ? 0 + (dateCheckIn.getMonth() + 1).toString()
+      : dateCheckIn.getMonth() + 1;
+  const day =
+    dateCheckIn.getDate() < 10
+      ? 0 + dateCheckIn.getDate().toString()
+      : dateCheckIn.getDate();
 
-    return `${year}-${month}-${day}`
-}
+  return `${year}-${month}-${day}`;
+};
--- src/saga/hotelsSaga.tsx
+++ src/saga/hotelsSaga.tsx
@@ -1,21 +1,24 @@
-import { put, takeEvery, call } from 'redux-saga/effects'
-import { fetchHotels } from '../otherFuncs/request'
-import { setHotels } from '../store/actionsCreater';
+import { put, takeEvery, call } from "redux-saga/effects";
+import { fetchHotels } from "../otherFuncs/request";
+import { setHotels } from "../store/actionsCreater";
 
-import store from '../store';
-import { Action } from '../store/mainReducer';
+import store from "../store";
+import { Action } from "../store/mainReducer";
 
 function* fetchHotelsWorker() {
-    const choosenCity = store.getState().choosenCity;
-    const checkIn = store.getState().checkIn;
-    const checkOut = store.getState().checkOut
+  const choosenCity = store.getState().choosenCity;
+  const checkIn = store.getState().checkIn;
+  const checkOut = store.getState().checkOut;
 
-    const data: Action = yield call(() => fetchHotels(choosenCity, checkIn, checkOut));
+  console.log(checkIn, checkOut);
 
-    yield put(setHotels(data))
+  const data: Action = yield call(() =>
+    fetchHotels(choosenCity, checkIn, checkOut)
+  );
 
+  yield put(setHotels(data));
 }
 
 export function* hotelsWatcher() {
-    yield takeEvery('FETCH_HOTELS', fetchHotelsWorker)
+  yield takeEvery("FETCH_HOTELS", fetchHotelsWorker);
 }
